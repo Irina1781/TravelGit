8a6cad5d13ed59f69ba54c6044bde389930fad83
Artem Zelenov <artem.zelenov@icloud.com>
2025-02-07 16:31:00 +0600

    chore: add comment to drizzle config

--- back/drizzle.config.js
+++ back/drizzle.config.js
@@ -6,7 +6,7 @@ console.log( process.env.DATABASE_URL)
 export default defineConfig({
   out: './drizzle',
   schema: './database/schema.js',
-  dialect: 'postgresql',
+  dialect: 'postgresql', // чтобы подключить mysql мы просто указываем тут mysql и всё, код самого приложения менять не надо
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
